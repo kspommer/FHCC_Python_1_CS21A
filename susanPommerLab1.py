ff17f92beb0b8811bd131a62b42bd16898853377
Susan Pommer <kspommer@gmail.com>
2018-07-29 11:51:37 -0700

    FHCC Python 1 CS21A (Summer 2017) - Assignment 1 Submission

---
+++ susanPommerLab1.py
@@ -0,0 +1,44 @@
+###########################################################
+# Course:  CS 21A Python Programming: Lab #1
+# Name:  Susan Pommer
+# Description: Hello World Program
+# File name:  Pommer_hello.py
+# Date:  July 10, 2017
+###########################################################
+
+# Introduction Statement
+print("Hello World!\n")
+
+# Personal Information
+print("My family name is Pommer. \nMy student ID is 10810822. \n")
+
+# Syllabus Details
+print("Key syllabus details are: \
+\n1. Lots of help is available! \
+\n2. Exam periods generously span 30 hours, supporting working students.\n")
+
+# Key Coding Lab Standards
+print("Important Coding Lab Standards are:\
+\n1. Always include a program header and comments. \
+\n2. Proper indents are critical and always four spaces wide. \
+\n3. As demonstrated in this program, source code lines should always \
+be less than 79 characters long.")
+
+
+# SAMPLE RUN #
+
+#Hello World!
+
+#My family name is Pommer. 
+#My student ID is 10810822.
+
+#Key syllabus details are: 
+#1. Lots of help is available! 
+#2. Exam periods generously span 30 hours, supporting working students.
+
+#Important Coding Lab Standards are:
+#1. Always include a program header and comments. 
+#2. Proper indents are critical and always four spaces wide. 
+#3. As demonstrated in this program, source code lines should always be less than 79 characters long.
+
+# SAMPLE RUN #
